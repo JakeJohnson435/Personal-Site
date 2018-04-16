8b85f77884c07ab4681d295a770220e6638a9302
Jake J <jakejohnson818@gmail.com>
2018-04-16 16:52:49 -0500

    Basic structure done, working on an issue with the navlinks

---
+++ PS-JS.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(".main").fadeToggle(1500);
+    $(".second").delay(1750).slideDown(500);
+    $(".main").delay(2500).fadeToggle(1000);
+    $(".second").delay(1600).fadeToggle(1000);
+    $(".third").delay(5000).fadeToggle(1500);
+    $(".links").delay(6700).fadeToggle(1000);
+});
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("nav");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
